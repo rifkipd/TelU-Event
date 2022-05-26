174e050cc9307f8b5ce198af75885747069ca633
Rifki Padilah <rpadilah90@gmail.com>
2022-05-27 00:30:28 +0700

    fix

--- app/src/main/java/org/tryhard/teluevent/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/org/tryhard/teluevent/ui/auth/signin/SignInFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import org.tryhard.teluevent.R
 import org.tryhard.teluevent.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
 
     private lateinit var binding: FragmentSignInBinding
+    private lateinit var nav: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -18,6 +21,16 @@ class SignInFragment : Fragment() {
         binding = FragmentSignInBinding.inflate(layoutInflater,container,false)
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nav = Navigation.findNavController(view)
+        binding.btnSignup2.setOnClickListener {
+            nav.navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
     }
 
 
--- app/src/main/java/org/tryhard/teluevent/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/org/tryhard/teluevent/ui/splashscreen/SplashScreenActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import org.tryhard.teluevent.MainActivity
 import org.tryhard.teluevent.R
+import org.tryhard.teluevent.ui.auth.AuthActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, AuthActivity::class.java))
         },3000)
     }
 }
