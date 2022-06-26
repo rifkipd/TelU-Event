f8f8ecf192dd2e1c19ca697c0f0504fbc455a75f
rifkipd <rpadilah90@gmail.com>
2022-06-26 23:39:36 +0700

    access-level login

---
+++ app/src/main/java/org/tryhard/teluevent/ui/auth/admin/AdminActivity.kt
@@ -0,0 +1,12 @@
+package org.tryhard.teluevent.ui.auth.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.tryhard.teluevent.R
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
+}
--- app/src/main/java/org/tryhard/teluevent/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/org/tryhard/teluevent/ui/auth/signup/SignUpFragment.kt
@@ -4,22 +4,40 @@ package org.tryhard.teluevent.ui.auth.signup
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.model.ObjectValue
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+import kotlinx.android.synthetic.main.fragment_sign_up.view.*
+import org.tryhard.teluevent.MainActivity
 import org.tryhard.teluevent.R
 import org.tryhard.teluevent.databinding.FragmentSignUpBinding
-import org.tryhard.teluevent.ui.auth.signin.SignInFragment
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class SignUpFragment : Fragment() {
     private lateinit var nav:NavController
     private lateinit var binding:FragmentSignUpBinding
     lateinit var auth:FirebaseAuth
+    lateinit var  fStore:FirebaseFirestore
+    lateinit var df:DocumentReference
+    private lateinit var user:FirebaseUser
+
+
+
+
+
 
 
     override fun onCreateView(
@@ -38,12 +56,19 @@ class SignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
         auth = FirebaseAuth.getInstance()
+        fStore = FirebaseFirestore.getInstance()
+
 
         binding.btnSignUpRegister.setOnClickListener { register() }
     }
 
     private fun register(){
+
+
+
         val email = binding.usernameInp.text.toString()
         val password = binding.passwordInp.text.toString()
 
@@ -82,12 +107,26 @@ class SignUpFragment : Fragment() {
 
     private fun RegisterFirebase(email: String, password: String) {
 
+
         auth.createUserWithEmailAndPassword(email , password)
             .addOnCompleteListener { (this)
+
                 if (it.isSuccessful){
+
+                    user = auth.currentUser!!
                     Toast.makeText(getContext(), "Registrasi Berhasil", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(getContext(),SignInFragment::class.java)
+                    df = fStore.collection("Users").document(user.uid)
+                    val userInfo:MutableMap<String,Any> = HashMap()
+                    userInfo["Fullname"] = fullname_inp.text.toString()
+                    userInfo["isUser"] = "1"
+                    df.set(userInfo)
+
+
+                    //specify if the user is Admin
+
+                    val intent = Intent(activity,MainActivity::class.java)
                     startActivity(intent)
+                    activity?.finish()
 
                 }else{
                     Toast.makeText(getContext(), "${it.exception?.message}", Toast.LENGTH_SHORT).show()
@@ -96,4 +135,10 @@ class SignUpFragment : Fragment() {
     }
 
 
+
+
+
 }
+
+
+
